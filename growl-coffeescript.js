58b0e95d660680b4f1734e76b345a36ea0a9c09c
Wes Bos <wesbos@gmail.com>
2011-07-19 01:29:05 -0400

    CoffeeScript in ur growl

---
+++ growl-coffeescript.js
@@ -0,0 +1,21 @@
+(function() {
+  var App, CoffeeScript, exec, sys;
+  sys = require('sys');
+  exec = require('child_process').exec;
+  CoffeeScript = require("coffee-script");
+  App = {};
+  App.puts = function(error, stdout, stderr) {
+    return sys.puts(stdout);
+  };
+  App.icon = "" + __dirname + "/i/coffee.png";
+  App.notify = function(message, type) {
+    exec("growlnotify  -m '" + message + "'	--image '" + __dirname + "/i/coffee-" + type + ".png'", App.puts);
+    return console.log("" + message);
+  };
+  CoffeeScript.on('failure', function(error, task) {
+    return App.notify("" + error, "error");
+  });
+  CoffeeScript.on('success', function(task) {
+    return App.notify("" + task.file + " sucessfully compiled!", "success");
+  });
+}).call(this);
